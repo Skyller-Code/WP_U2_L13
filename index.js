f20fcbc60390770979c7a95d0c1f4a3f84fd4e6d
Skyller <smbaughman08@outlook.com>
2025-11-19 13:36:37 -0500

    added to js

---
+++ index.js
@@ -0,0 +1,14 @@
+function flip() //its called flip because when a card is clicked it will flip showing its other side
+{
+    //wip
+}
+
+function addImg()
+{
+    const deck = document.getElementsByClassName("blocksinside");
+
+    for(let img = 0; img > deck.length; img++)
+    {
+        console.log(deck[img]); //last left off
+    }
+}
